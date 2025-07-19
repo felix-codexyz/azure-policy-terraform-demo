70030f35b672566b5cd8da48308600602fe19abc
Felix M <felixmomodebe@gmail.com>
2025-07-19 18:52:26 -0400

    Updated backend block

--- policy/main.tf
+++ policy/main.tf
@@ -1,81 +1,36 @@
 terraform {
-       required_providers {
-         azurerm = {
-           source  = "hashicorp/azurerm"
-           version = "~>3.0"
-         }
-       }
-       terraform { 
-  cloud { 
-    
-    organization = "felfun-spz-technologies-azure-platform" 
-
-    workspaces { 
-      name = "azure-policy-workspace" 
-    } 
-  } 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  cloud {
+    organization = "felfun-spz-technologies-azure-platform"
+    workspaces {
+      name = "azure-policy-workspace"
+    }
+  }
 }
-       terraform { 
-  cloud { 
-    
-    organization = "felfun-spz-technologies-azure-platform" 
 
-    workspaces { 
-      name = "azure-policy-workspace" 
-    } 
-  } 
+provider "azurerm" {
+  features {}
 }
-       terraform { 
-  cloud { 
-    
-    organization = "felfun-spz-technologies-azure-platform" 
 
-    workspaces { 
-      name = "azure-policy-workspace" 
-    } 
-  } 
+data "azurerm_subscription" "current" {}
+
+resource "azurerm_policy_definition" "restrict_location" {
+  name         = var.policy_name
+  display_name = var.policy_display_name
+  policy_type  = "Custom"
+  mode         = "All"
+  policy_rule  = file("${path.module}/restrict-region-policy.json")
 }
-       terraform { 
-  cloud { 
-    
-    organization = "felfun-spz-technologies-azure-platform" 
 
-    workspaces { 
-      name = "azure-policy-workspace" 
-    } 
-  } 
+resource "azurerm_subscription_policy_assignment" "restrict_location_assignment" {
+  name                 = var.policy_assignment_name
+  subscription_id      = data.azurerm_subscription.current.id
+  policy_definition_id = azurerm_policy_definition.restrict_location.id
+  description          = var.policy_assignment_description
+  display_name         = var.policy_assignment_display_name
 }
-       terraform { 
-  cloud { 
-    
-    organization = "felfun-spz-technologies-azure-platform" 
-
-    workspaces { 
-      name = "azure-policy-workspace" 
-    } 
-  } 
-}
-       }
-     }
-
-     provider "azurerm" {
-       features {}
-     }
-
-     data "azurerm_subscription" "current" {}
-
-     resource "azurerm_policy_definition" "restrict_location" {
-       name         = var.policy_name
-       display_name = var.policy_display_name
-       policy_type  = "Custom"
-       mode         = "All"
-       policy_rule  = file("${path.module}/restrict-region-policy.json")
-     }
-
-     resource "azurerm_subscription_policy_assignment" "restrict_location_assignment" {
-       name                 = var.policy_assignment_name
-       subscription_id      = data.azurerm_subscription.current.id
-       policy_definition_id = azurerm_policy_definition.restrict_location.id
-       description          = var.policy_assignment_description
-       display_name         = var.policy_assignment_display_name
-     }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,81 +1,27 @@
 terraform {
-       required_providers {
-         azurerm = {
-           source  = "hashicorp/azurerm"
-           version = "~>3.0"
-         }
-       }
-       terraform { 
-  cloud { 
-    
-    organization = "felfun-spz-technologies-azure-platform" 
-
-    workspaces { 
-      name = "azure-storage-workspace" 
-    } 
-  } 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  cloud {
+    organization = "felfun-spz-technologies-azure-platform"
+    workspaces {
+      name = "azure-policy-workspace"
+    }
+  }
 }
-       terraform { 
-  cloud { 
-    
-    organization = "felfun-spz-technologies-azure-platform" 
 
-    workspaces { 
-      name = "azure-policy-workspace" 
-    } 
-  } 
+provider "azurerm" {
+  features {}
 }
-       terraform { 
-  cloud { 
-    
-    organization = "felfun-spz-technologies-azure-platform" 
 
-    workspaces { 
-      name = "azure-policy-workspace" 
-    } 
-  } 
+module "storage_account" {
+  source                   = "./modules/storage-account"
+  resource_group_name      = var.resource_group_name
+  storage_account_name     = var.storage_account_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
 }
-       terraform { 
-  cloud { 
-    
-    organization = "felfun-spz-technologies-azure-platform" 
-
-    workspaces { 
-      name = "azure-policy-workspace" 
-    } 
-  } 
-}
-       terraform { 
-  cloud { 
-    
-    organization = "felfun-spz-technologies-azure-platform" 
-
-    workspaces { 
-      name = "azure-policy-workspace" 
-    } 
-  } 
-}
-       terraform { 
-  cloud { 
-    
-    organization = "felfun-spz-technologies-azure-platform" 
-
-    workspaces { 
-      name = "azure-policy-workspace" 
-    } 
-  } 
-}
-     }
-
-     provider "azurerm" {
-       features {}
-     }
-
-     module "storage_account" {
-       source                   = "./modules/storage-account"
-       resource_group_name      = var.resource_group_name
-       storage_account_name     = var.storage_account_name
-       location                 = var.location
-       account_tier             = var.account_tier
-       account_replication_type = var.account_replication_type
-     }
